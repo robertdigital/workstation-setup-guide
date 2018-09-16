9078a99758c8ba74514ae4e6e04cd7a1bc74781d
Timothy Liu <tlkh@live.co.uk>
2018-09-17 00:17:52 +0800

    Updated demo spawner config with nicer UX

--- kubeflow_files/demo_jupyterhub_config.py
+++ kubeflow_files/demo_jupyterhub_config.py
@@ -1,82 +1,84 @@
 import json
 import os
+import logging
 from kubespawner.spawner import KubeSpawner
-
 
 class KubeFormSpawner(KubeSpawner):
 
     def _options_form_default(self):
         global registry, repoName
         return '''
-    <h2>Kubeflow Demo</h2>
-    <p>Please allocate resources responsibly<br>
-    If your server is unable to start, it is likely that the GPU you requested for is not available.<br>
-    Try starting a server <b>without a GPU</b> before reporting a server problem.</p>
-    <hr>
-    <label for='image'>Image</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input list=\"image\" name=\"image\" placeholder='repo/image:tag'>
-    <datalist id=\"image\">
-      <option value=\"jupyter/minimal-notebook:latest\">
-      <option value=\"jupyter/datascience-notebook:latest\">
-      <option value=\"stepankuzmin/pytorch-notebook:latest\">
-      <option value=\"{0}/{1}/tensorflow-1.7.0-notebook-cpu:v0.2.1\">
-      <option value=\"{0}/{1}/tensorflow-1.7.0-notebook-gpu:v0.2.1\">
-      <option value=\"{0}/{1}/tensorflow-1.8.0-notebook-cpu:v0.2.1\">
-      <option value=\"{0}/{1}/tensorflow-1.8.0-notebook-gpu:v0.2.1\">
-    </datalist>
-    <p>If image is not present, it will be pulled from Docker Hub.<br>This may take a long time to complete.</p>
-    <br/>
-    <label for='cpu_guarantee'>CPU</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input name='cpu_guarantee' placeholder='1.0'></input>
-    <p>If your work is not CPU-intensive, you can choose less than 1 CPU (e.g. <code>0.5</code>)</p>
-    <br/>
-    <label for='mem_guarantee'>Memory</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input name='mem_guarantee' placeholder='1.0Gi'></input>
-    <br/>
-    <label for='extra_resource_limits'>GPU</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input list=\"extra_resource_limits\" name=\"extra_resource_limits\" placeholder=''>
+    <script defer src='https://use.fontawesome.com/releases/v5.3.1/js/solid.js' integrity='sha384-GJiigN/ef2B3HMj0haY+eMmG4EIIrhWgGJ2Rv0IaWnNdWdbWPr1sRLkGz7xfjOFw' crossorigin='anonymous'></script>
+    <script defer src='https://use.fontawesome.com/releases/v5.3.1/js/brands.js' integrity='sha384-2vdvXGQdnt+ze3ylY5ESeZ9TOxwxlOsldUzQBwtjvRpen1FwDT767SqyVbYrltjb' crossorigin='anonymous'></script>
+    <script defer src='https://use.fontawesome.com/releases/v5.3.1/js/fontawesome.js' integrity='sha384-2OfHGv4zQZxcNK+oL8TR9pA+ADXtUODqGpIRy1zOgioC4X3+2vbOAp5Qv7uHM4Z8' crossorigin='anonymous'></script>
+    <div class='card' style='width: 100%;'>
+    <div class='card-body'>
+        <h3 class='card-title'>Welcome to Demo JupyterHub</h3>
+        <h4 class='card-subtitle mb-2 text-muted'>Easy Jupyter-based GPU Compute</h4>
+        <p class='card-text'>
+        <ul>
+            <li>Hello world!</li>
+        </ul>
+        </p>
+        <a target='_blank' class='btn btn-primary' href='#'>Random Button</a> 
+    </div>
+    </div>
+    <br>
+    <div class='form-group'>
+    <label for='inputDockerImage'><i class='fab fa-docker'></i> Docker Image</label>
+    <input type='text' class='form-control' id='inputDockerImage' aria-describedby='imgHelp' placeholder='tlkh/deeplearning-lab:0.2' value='tlkh/deeplearning-lab:0.2'>
+    <p><small id='imgHelp' class='form-text text-muted'>You can use any JupyterHub compatible Docker image from Docker Hub.<br>
+    The default image is <code>tlkh/deeplearning-lab:0.2</code>, which includes most typical packages required for deep learning: TensorFlow, Keras, PyTorch and OpenCV.</small></p>
+    </div>
+    <div class='form-group'>
+    <label for='cpu_guarantee'><i class='fas fa-microchip'></i> vCPU Threads</label>
+    <input type='text' class='form-control' id='cpu_guarantee' aria-describedby='cpuHelp' placeholder='1.0'>
+    <p><small id='cpuHelp' class='form-text text-muted'>If your work is not CPU-intensive, you can choose less than 1 CPU (e.g. <code>0.5</code>)</small></p>
+    </div>
+    <div class='form-group'>
+    <label for='mem_guarantee'><i class='fas fa-memory'></i> Reserved Memory</label>
+    <input type='text' class='form-control' id='mem_guarantee' aria-describedby='memHelp' placeholder='1.0Gi'>
+    <p><small id='memHelp' class='form-text text-muted'>Total amount of reserved RAM (e.g. <code>7.5Gi</code>). Your Notebook may consume more if system resources allow.</small></p>
+    </div>
+    <div class='form-group'>
+    <label for='extra_resource_limits'><i class='fas fa-ticket-alt'></i> GPU</label>
+    <input class='form-control' list=\"extra_resource_limits\" name=\"extra_resource_limits\" placeholder=''>
     <datalist id=\"extra_resource_limits\">
-      <option value=\"\">
+      <option value=\"{{&quot;nvidia.com/gpu&quot;: 0}}\">
       <option value=\"{{&quot;nvidia.com/gpu&quot;: 1}}\">
       <option value=\"{{&quot;nvidia.com/gpu&quot;: 2}}\">
       <option value=\"{{&quot;nvidia.com/gpu&quot;: 3}}\">
       <option value=\"{{&quot;nvidia.com/gpu&quot;: 4}}\">
     </datalist>
-    <p>Example: to request 2 GPUs: <code>{{&quot;nvidia.com/gpu&quot;: 2}}</code><br>Please leave this field blank for none.</p>
-    <br/>
-    <label for='node_selector'>Resource Criteria</label>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <input list=\"node_selector\" name=\"node_selector\" placeholder=''>
-    <datalist id=\"node_selector\">
-      <option value=\"\">
-      <option value=\"{{&quot;accelerator&quot;: &quot;k80&quot;}}\">
-      <option value=\"{{&quot;accelerator&quot;: &quot;p100&quot;}}\">
-    </datalist>
-    <p>To request specifically for P100: <code>{{&quot;accelerator&quot;: &quot;p100&quot;}}</code><br>Options: p100, k80, or leave it blank</p>
-    <br/>
+    <p><small id='memHelp'>Example: to reserve 2 GPUs: <code>{{&quot;nvidia.com/gpu&quot;: 2}}</code><br>
+    To reserve no GPU, please use <code>{{&quot;nvidia.com/gpu&quot;: 0}}</code>.</small></p>
+    </div>
+    <br>
+    <div class='alert alert-warning' role='alert'>
+    <p>If your Notebook is unable to start, it is likely that the GPU you requested for is not available.<br>
+    Try starting a Notebook <b>without a GPU</b> before reporting a server problem!</p>
+    <div>
+    <br>
     '''.format(registry, repoName)
 
     def options_from_form(self, formdata):
         options = {}
-        options['image'] = formdata.get('image', [''])[0].strip()
+        options['image'] = formdata.get('inputDockerImage', [''])[0].strip()
         options['cpu_guarantee'] = formdata.get(
             'cpu_guarantee', [''])[0].strip()
         options['mem_guarantee'] = formdata.get(
             'mem_guarantee', [''])[0].strip()
         options['extra_resource_limits'] = formdata.get(
             'extra_resource_limits', [''])[0].strip()
-        options['node_selector'] = formdata.get(
-            'node_selector', [''])[0].strip()
+        logging.info(options)
         return options
 
     @property
     def singleuser_image_spec(self):
-        global cloud
-        if cloud == 'ack':
-            image = 'registry.aliyuncs.com/kubeflow-images-public/tensorflow-notebook-cpu'
-        else:
-            image = 'gcr.io/kubeflow-images-public/tensorflow-1.8.0-notebook-cpu:v0.2.1'
         if self.user_options.get('image'):
             image = self.user_options['image']
+        else:
+            image = 'tlkh/deeplearning-lab:0.2'
         return image
 
     @property
@@ -100,12 +102,6 @@ class KubeFormSpawner(KubeSpawner):
             extra = json.loads(self.user_options['extra_resource_limits'])
         return extra
 
-    @property
-    def node_selector(self):
-        extra = ''
-        if self.user_options.get('node_selector'):
-            extra = json.loads(self.user_options['node_selector'])
-        return extra
 
 
 ###################################################
@@ -165,14 +161,17 @@ pvc_mount = os.environ.get('NOTEBOOK_PVC_MOUNT')
 if pvc_mount and pvc_mount != 'null':
     c.KubeSpawner.user_storage_pvc_ensure = True
     # How much disk space do we want?
-    c.KubeSpawner.user_storage_capacity = '1Gi'
+    c.KubeSpawner.user_storage_capacity = '50Gi'
     c.KubeSpawner.user_storage_class = 'standard'
+    c.KubeSpawner.user_storage_access_modes = ['ReadWriteMany']
     c.KubeSpawner.pvc_name_template = 'claim-{username}{servername}'
+
+    # user volume
     volumes.append(
         {
             'name': 'volume-{username}{servername}',
             'persistentVolumeClaim': {
-                'claimName': 'claim-{username}{servername}'
+                    'claimName': 'claim-{username}{servername}'
             }
         }
     )
@@ -180,6 +179,22 @@ if pvc_mount and pvc_mount != 'null':
         {
             'mountPath': pvc_mount,
             'name': 'volume-{username}{servername}'
+        }
+    )
+
+    # dataset volume
+    volumes.append(
+        {
+            'name': 'volume-datasets',
+            'persistentVolumeClaim': {
+                    'claimName': 'claim-datasets'
+            }
+        }
+    )
+    volume_mounts.append(
+        {
+            'mountPath': pvc_mount+'/datasets',
+            'name': 'volume-datasets'
         }
     )
 
